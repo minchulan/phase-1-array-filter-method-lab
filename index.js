2a3cbb77704b8206490f15690dba5cab3228d2be
Minchul An <min@minchulan.io>
2022-09-28 00:14:05 -0400

    commit tree 95b4faeb34751b7ee04fd2215a89344ecdbbe91e

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Code your solution here
+// findMatching()
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+  // drivers.filter(function (driver) {
+  //     return driver === name;
+  // })
+}
+
+//fuzzyMatch()
+// strings and arrays have overlapping methods: split(), join(), slice()
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.slice(0, letters.length) === letters)
+}
+
+//matchName()
+function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name.toLowerCase === name.toLowerCase())
+}
+
+// const drivers = [
+//   {
+//     name: "Bobby",
+//     hometown: "Pittsburgh",
+//   },
+//   {
+//     name: "Sammy",
+//     hometown: "New York",
+//   },
+//   {
+//     name: "Sally",
+//     hometown: "Cleveland",
+//   },
+//   {
+//     name: "Annette",
+//     hometown: "Los Angeles",
+//   },
+//   {
+//     name: "Bobby",
+//     hometown: "Tampa Bay",
+//   },
+// ];
